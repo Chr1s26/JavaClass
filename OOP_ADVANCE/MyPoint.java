796aadfada16087c330c98cd1b83b53220bfb283
Htet Kyaw Swar Linn <160567260+Chr1s26@users.noreply.github.com>
2024-06-27 00:59:02 +0700

    Update MyPoint.java

--- OOP_ADVANCE/MyPoint.java
+++ OOP_ADVANCE/MyPoint.java
@@ -50,8 +50,6 @@ public class MyPoint {
 	public double distance(MyPoint another) {
 		return Math.sqrt(Math.pow(this.x - another.x, 2) + Math.pow(this.y - another.y, 2));
 	}
-	//this.x nk another.x nk br kwr
-	//this.x ka current pointer new object yae x .another ka ll another object yae x. 
-	//distance parameter nha khu yae usage ka br so lo tr ll. which one is better.
+	
 
 }
